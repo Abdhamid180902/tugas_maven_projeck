360310b2b4dda4013d3a656415f6b064108753a0
Abdhamid180902 <114447409+Abdhamid180902@users.noreply.github.com>
2022-12-19 22:24:33 -0800

    Add files via upload

---
+++ hamid_projeck/src/test/java/desainer/DriverTest.java
@@ -0,0 +1,21 @@
+package desainer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.sql.SQLException;
+
+public class DriverTest {
+    @Test
+
+    void testRegistrasi(){
+        try {
+            Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(mysqlDriver);
+
+        }catch (SQLException exception){
+            Assertions.fail(exception);
+        }
+    }
+}
